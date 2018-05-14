2384ef0c57671e56114cdcf249884c0a6cf3abae
Bele <jorgebelena@gmail.com>
2018-05-14 21:15:37 +0200

    Commited code for lesson 1
    Updated gitignore, Readme, and added code for lesson 1 of the Course

---
+++ Lesson_1_Data_Extraction_Fundamentals/11-Reading_Excel_Files/readxls.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+"""
+Your task is as follows:
+- read the provided Excel file
+- find and return the min and max values for the COAST region
+- find and return the time value for the min and max entries
+- the time values should be returned as Python tuples
+
+Please see the test function for the expected return format
+"""
+
+import xlrd
+from zipfile import ZipFile
+datafile = "2013-ercot-hourly-load-data"
+
+
+def open_zip(datafile):
+    print ('{0}.zip'.format(datafile) + ' aaaa')
+    with ZipFile('{0}.zip'.format(datafile), 'r') as myzip:
+        myzip.extractall()
+
+
+def parse_file(datafile):
+    workbook = xlrd.open_workbook(datafile)
+    sheet = workbook.sheet_by_index(0)
+
+    # we just want the region COAST, that is column[1] in the excel sheet
+    sheet_data = [[sheet.cell_value(r, col) for col in 1] for r in range(sheet.nrows)]
+
+    print(sheet_data)
+    ### other useful methods:
+    # print "\nROWS, COLUMNS, and CELLS:"
+    # print "Number of rows in the sheet:", 
+    # print sheet.nrows
+    # print "Type of data in cell (row 3, col 2):", 
+    # print sheet.cell_type(3, 2)
+    # print "Value in cell (row 3, col 2):", 
+    # print sheet.cell_value(3, 2)
+    # print "Get a slice of values in column 3, from rows 1-3:"
+    # print sheet.col_values(3, start_rowx=1, end_rowx=4)
+
+    # print "\nDATES:"
+    # print "Type of data in cell (row 1, col 0):", 
+    # print sheet.cell_type(1, 0)
+    # exceltime = sheet.cell_value(1, 0)
+    # print "Time in Excel format:",
+    # print exceltime
+    # print "Convert time to a Python datetime tuple, from the Excel float:",
+    # print xlrd.xldate_as_tuple(exceltime, 0)
+    
+    
+    data = {
+            'maxtime': (0, 0, 0, 0, 0, 0),
+            'maxvalue': 0,
+            'mintime': (0, 0, 0, 0, 0, 0),
+            'minvalue': 0,
+            'avgcoast': 0
+    }
+    return data
+
+
+def test():
+    open_zip(datafile)
+    data = parse_file(datafile)
+
+    assert data['maxtime'] == (2013, 8, 13, 17, 0, 0)
+    assert round(data['maxvalue'], 10) == round(18779.02551, 10)
+
+
+test()
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# uc-dw-mongodb
-Udacity Course Data Wrangling with MongoDB
+### **U**dacity **C**ourse **-** **D**ata **W**rangling with **MongoDB**
+
+#### [*(uc-dw-mongodb)*](https://github.com/jbelenag/uc-dw-mongodb) *by jbelenag*
+
+You'll find the Course Material here https://www.udacity.com/wiki/ud032
